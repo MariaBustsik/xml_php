1a298054cdab4833c799add182981b40c488749d
Maria Bustsik <maria.bustsik@outlook.com>
2021-11-03 21:48:41 +0200

    xml eraldi faili

--- mets.php
+++ mets.php
@@ -1,32 +1,10 @@
-<?php
-if(isset($_POST['submit'])){
-    $xmlDoc = new DOMDocument("1.0","UTF-8");
-    $xmlDoc->formatOutput = true;
-    $xmlDoc->preserveWhiteSpace = false;
-}
-?>
 
 <?php
-if(isset($_POST['submit'])){
-    $xmlDoc = new DOMDocument("1.0","UTF-8");
-    $xmlDoc->formatOutput = true;
-    $xmlDoc->preserveWhiteSpace = false;
-
-    $xml_root = $xmlDoc->createElement("tooted");
-    $xmlDoc->appendChild($xml_root);
-
-    $xml_toode = $xmlDoc->createElement("toode");
-    $xmlDoc->appendChild($xml_toode);
-
-    $xml_root->appendChild($xml_toode);
-}
-?>
-
-<?php
+$andmed=simplexml_load_file('andmeteBaas.xml');
 if(isset($_POST['submit'])){
     $xmlDoc = new DOMDocument("1.0","UTF-8");
     $xmlDoc->preserveWhiteSpace = false;
-    $xmlDoc->load('tooted.xml');
+    $xmlDoc->load('andmeteBaas.xml');
     $xmlDoc->formatOutput = true;
 
     $xml_root = $xmlDoc->documentElement;
@@ -34,52 +12,80 @@ if(isset($_POST['submit'])){
 
     $xml_toode = $xmlDoc->createElement("toode");
     $xmlDoc->appendChild($xml_toode);
-
     $xml_root->appendChild($xml_toode);
-
+    $xml_lisad = $xmlDoc->createElement("lisad");
+    $xml_toode->appendChild($xml_lisad);
     unset($_POST['submit']);
     foreach($_POST as $voti=>$vaartus){
-        $kirje = $xmlDoc->createElement($voti,$vaartus);
-        $xml_toode->appendChild($kirje);
+         if ($voti == "lnimi" or $voti == "suurus") {
+
+             $kirje = $xmlDoc->createElement($voti,$vaartus);
+             $xml_lisad->appendChild($kirje);
+
+        }
+         else {
+             $kirje = $xmlDoc->createElement($voti,$vaartus);
+             $xml_toode->appendChild($kirje);
+         }
+
     }
-    $xmlDoc->save('tooted.xml');
+    $xmlDoc->save('andmeteBaas.xml');
+    header("refresh: 0;");
 }
 ?>
 
 
 <!DOCTYPE html>
-<html>
-
+<html lang="et">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Toote lisamine</title>
+    <title>XML andmete lugemine PHP abil</title>
 </head>
-
 <body>
-<h2>Toote sisestamine</h2>
+<h1>XML andmete lugemine PHP abil</h1>
+<h3>Esimese toode nimi:</h3>
+<?php
+echo $andmed->toode[0]->nimi;
+?>
 <table>
-    <form action="" method="post" name="vorm1">
-        <tr>
-            <td><label for="nimetus">Toote nimetus:</label></td>
-            <td><input type="text" name="nimetus" id="nimetus" autofocus></td>
-        </tr>
-        <tr>
-            <td><label for="kirjeldus">Kirjeldus:</label></td>
-            <td><input type="text" name="kirjeldus" id="kirjeldus"></td>
-        </tr>
-        <tr>
-            <td><label for="hind">Hind:</label></td>
-            <td><input type="text" name="hind" id="hind"></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="submit" id="submit" value="Sisesta"></td>
-            <td></td>
-        </tr>
-    </form>
+    <tr>
+        <th>Toode nimi</th>
+        <th>Hind</th>
+        <th>Värv</th>
+        <th>Lisade nimi</th>
+        <th>Lisade suurus</th>
+    </tr>
+    <?php
+    foreach ($andmed->toode as $toode){
+        echo "<tr>";
+        echo "<td>".($toode->nimi)."</td>";
+        echo "<td>".($toode->hind)."</td>";
+        echo "<td>".($toode->varv)."</td>";
+        echo "<td>".($toode->lisad->lnimi)."</td>";
+        echo "<td>".($toode->lisad->suurus)."</td>";
+        echo "</tr>";
+    }
+    ?>
 </table>
 
+<h1>Vormista saadud andmete lisamine XML faili</h1>
+<form method="post" action="">
+    <label for="nimi">Toode nimi</label>
+    <input type="text" id="nimi" name="nimi">
+    <br>
+    <label for="hind">Toode hind</label>
+    <input type="text" id="hind" name="hind">
+    <br>
+    <label for="varv">Toode värv</label>
+    <input type="text" id="varv" name="varv">
+    <br>
+    <label for="lnimi">Lisade nimi</label>
+    <input type="text" id="lnimi" name="lnimi">
+    <br>
+    <label for="suurus">Lisade suurus</label>
+    <input type="text" id="suurus" name="suurus">
+    <input type="submit" value="Sisesta" id="submit" name="submit">
 
+</form>
 </body>
 </html>
 
--- mets2.php
+++ mets2.php
@@ -1,4 +1,7 @@
+
 <?php
+$andmed=simplexml_load_file('lingid.xml');
+
 if(isset($_POST['submit'])){
     $xmlDoc = new DOMDocument("1.0","UTF-8");
     $xmlDoc->formatOutput = true;
@@ -7,15 +10,55 @@ if(isset($_POST['submit'])){
     $xml_root = $xmlDoc->createElement("tooted");
     $xmlDoc->appendChild($xml_root);
 
+
     $xml_toode = $xmlDoc->createElement("toode");
     $xmlDoc->appendChild($xml_toode);
 
     $xml_root->appendChild($xml_toode);
+    $xml_lisad = $xmlDoc->createElement("lisad");
+    $xml_toode->appendChild($xml_lisad);
 
-    $xml_toode->appendChild($xmlDoc->createElement('nimetus','muruniiduk'));
-    $xml_toode->appendChild($xmlDoc->createElement('kirjeldus','Väga lahe aparaat'));
 
-    $xmlDoc->save('tooted.xml');
+    $xmlDocc = new DOMDocument("1.0","UTF-8");
+    $xmlDocc->preserveWhiteSpace = false;
+    $xmlDocc->load('lingid.xml');
+    $xmlDocc->formatOutput = true;
+
+    $xml_roott = $xmlDocc->documentElement;
+    $xmlDocc->appendChild($xml_roott);
+
+    $xml_link = $xmlDocc->createElement("link");
+    $xmlDocc->appendChild($xml_link);
+    $xml_roott->appendChild($xml_link);
+
+    unset($_POST['submit']);
+    foreach($_POST as $voti=>$vaartus){
+        if ($voti == "fname") {
+
+            $xmlDocc->appendChild($xml_roott);
+            $kirjet = $xmlDocc->createElement($voti,$vaartus);
+            $xml_link->appendChild($kirjet);
+
+        }
+        else if ($voti == "lnimi" or $voti == "suurus") {
+
+            $kirje = $xmlDoc->createElement($voti,$vaartus);
+            $xml_lisad->appendChild($kirje);
+
+        }
+        else {
+            $kirje = $xmlDoc->createElement($voti,$vaartus);
+            $xml_toode->appendChild($kirje);
+
+        }
+    }
+
+
+    $fname=$_POST['fname'];
+
+    $xmlDocc->save("lingid.xml");
+    $xmlDoc->save($fname);
+    header("refresh: 0;");
 }
 ?>
 
@@ -37,18 +80,58 @@ if(isset($_POST['submit'])){
             <td><input type="text" name="nimetus" id="nimetus" autofocus></td>
         </tr>
         <tr>
-            <td><label for="kirjeldus">Kirjeldus:</label></td>
-            <td><input type="text" name="kirjeldus" id="kirjeldus"></td>
-        </tr>
-        <tr>
             <td><label for="hind">Hind:</label></td>
             <td><input type="text" name="hind" id="hind"></td>
+        </tr>
+        <tr>
+            <td><label for="varv">Värv:</label></td>
+            <td><input type="text" name="varv" id="varv"></td>
+        </tr>
+
+        <tr>
+            <td><label for="lnimi">Lisade nimi:</label></td>
+            <td><input type="text" name="lnimi" id="lnimi"></td>
+        </tr>
+
+        <tr>
+            <td><label for="suurus">Lisade suurus:</label></td>
+            <td><input type="text" name="suurus" id="suurus"></td>
+        </tr>
+
+        <tr>
+            <td><label for="fname">Faili nimi:</label></td>
+            <td><input type="text" name="fname" id="fname"></td>
         </tr>
         <tr>
             <td><input type="submit" name="submit" id="submit" value="Sisesta"></td>
             <td></td>
         </tr>
     </form>
+</table>
+
+<table>
+    <tr>
+        <th>Nimetus</th>
+        <th>Hind</th>
+        <th>Värv</th>
+        <th>Lisade nimi</th>
+        <th>Lisade suurus</th>
+    </tr>
+    <?php
+    foreach ($andmed->link as $link){
+
+        $andmedd = simplexml_load_file(($link->fname));
+    foreach ($andmedd->toode as $toode){
+        echo "<tr>";
+        echo "<td>".($toode->nimetus)."</td>";
+        echo "<td>".($toode->hind)."</td>";
+        echo "<td>".($toode->varv)."</td>";
+        echo "<td>".($toode->lisad->lnimi)."</td>";
+        echo "<td>".($toode->lisad->suurus)."</td>";
+        echo "</tr>";
+    }
+    }
+    ?>
 </table>
 </body>
 </html>
